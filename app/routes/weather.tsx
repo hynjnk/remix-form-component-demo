d820229730b0a264528a9ab6b40ecf5756bedb4a
Hyunjoon KIM <hyunjoon.kim@rightcode.co.jp>
2024-12-11 17:10:18 +0900

    replace useSearchParams to loader

--- app/routes/weather.tsx
+++ app/routes/weather.tsx
@@ -1,18 +1,25 @@
-import { Outlet, useSearchParams } from "@remix-run/react";
+import { LoaderFunctionArgs } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
 import { RemixForm } from "~/components/RemixForm";
 import { VanillaForm } from "~/components/VanillaForm";
 
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const city = url.searchParams.get("city");
+  // console.log("weather loader: city", city);
+  return { city };
+};
+
 export default function WeatherLayout() {
-  const [searchParams] = useSearchParams();
-  const city = searchParams.get("city") ?? undefined;
+  const { city } = useLoaderData<typeof loader>();
   return (
     <div className="flex">
       <aside className="w-72 p-4 bg-gray-100 flex flex-col gap-4">
         <h2 className="text-gray-700 text-lg font-semibold">
           app/routes/weather.tsx
         </h2>
-        <VanillaForm city={city} />
-        <RemixForm city={city} />
+        <VanillaForm city={city ?? undefined} />
+        <RemixForm city={city ?? undefined} />
       </aside>
       <Outlet />
     </div>
